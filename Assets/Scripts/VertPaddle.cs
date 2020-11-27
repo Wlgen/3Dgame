5bee30d35db21b2d4f635ad63f0e3cddcbf7d0bb
Iván Díaz <navi11031999@gmail.com>
2020-11-27 16:04:15 +0100

    a ver ahora que tal

---
+++ Assets/Scripts/VertPaddle.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VertPaddle : MonoBehaviour
+{
+    Rigidbody _rigidbody;
+    float _yspeed;
+
+    void Start()
+    {
+        _yspeed = 0.1f;
+        _rigidbody = GetComponent<Rigidbody>();
+    }
+
+    private void OnCollisionEnter(Collision collision) {
+        _yspeed = -_yspeed;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+
+        _rigidbody.AddForce(0,_yspeed,0,ForceMode.VelocityChange);
+    }
+}
