a646977e0a9a0a8cdc907c72a765f57a27d54d6d
Arnau <arnaugesapascual1999@gmail.com>
2020-11-28 13:24:26 +0100

    Rebote bola (un poco cumbia)

---
+++ Assets/Scripts/Ball.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour
+{
+    float _speedBall;
+    Rigidbody _rigidBody;
+    Vector3 _velocity;
+
+    void Start()
+    {
+        _speedBall = 10f;
+        _rigidBody = GetComponent<Rigidbody>();
+        _rigidBody.velocity = new Vector3(1f, 1f, 0f) * _speedBall;
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Debug.Log("entra");
+            _rigidBody.velocity = new Vector3(_rigidBody.velocity.x, -_rigidBody.velocity.y, _rigidBody.velocity.z) * _speedBall;
+        }
+    }
+    void FixedUpdate()
+    {
+        _rigidBody.velocity = _rigidBody.velocity.normalized * _speedBall;
+        _velocity = _rigidBody.velocity;
+    }
+
+    private void OnCollisionEnter(Collision collision) { 
+        _rigidBody.velocity = Vector3.Reflect(_velocity, collision.contacts[0].normal);
+    }
+}
