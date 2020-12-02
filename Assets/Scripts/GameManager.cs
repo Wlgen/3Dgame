6ceb5e81bb8231be1aed28f073756c3b2cca6f19
Arnau <arnaugesapascual1999@gmail.com>
2020-12-02 19:13:14 +0100

    Panels (de mica en mica s'omple la pica)

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,160 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+    public Text levelText;
+
+    public GameObject panelMenu;
+    public GameObject panelPlay;
+    public GameObject panelLevelCompleted;
+    public GameObject panelGameOver;
+    public GameObject panelInstructions;
+    public GameObject panelCredits;
+
+    public GameObject[] levels;
+
+    public static GameManager Instance { get; private set; }
+
+    public enum State { MENU, INIT, PLAY, LEVELCOMPLETED, LOADLEVEL, GAMEOVER }
+    State _state;
+    GameObject _currentBall;
+    GameObject _currentLevel;
+    bool _isSwitchingState;
+
+    private int _level;
+
+    public int Level
+    {
+        get { return _level; }
+        set
+        {
+            _level = value;
+            levelText.text = "LEVEL: " + _level;
+        }
+    }
+
+    public void PlayClicked()
+    {
+        SwitchState(State.INIT);
+    }
+
+    void Start()
+    {
+        Instance = this;
+        SwitchState(State.MENU);
+    }
+
+
+    public void SwitchState(State newState, float delay = 0)
+    {
+        StartCoroutine(SwitchDelay(newState, delay));
+    }
+
+    IEnumerator SwitchDelay(State newState, float delay)
+    {
+        _isSwitchingState = true;
+        yield return new WaitForSeconds(delay);
+        EndState();
+        _state = newState;
+        BeginState(newState);
+        _isSwitchingState = false;
+    }
+
+
+    void BeginState(State newState)
+    {
+        switch (newState)
+        {
+            case State.MENU:
+                Cursor.visible = true;
+                panelMenu.SetActive(true);
+                break;
+            case State.INIT:
+                Cursor.visible = false;
+                panelPlay.SetActive(true);
+                Level = 0;
+                if (_currentLevel != null)
+                {
+                    Destroy(_currentLevel);
+                }
+                SwitchState(State.LOADLEVEL);
+                break;
+            case State.PLAY:
+                break;
+            case State.LEVELCOMPLETED:
+                Destroy(_currentBall);
+                Destroy(_currentLevel);
+                Level++;
+                panelLevelCompleted.SetActive(true);
+                SwitchState(State.LOADLEVEL, 2f);
+                break;
+            case State.LOADLEVEL:
+                if (Level >= levels.Length)
+                {
+                    SwitchState(State.GAMEOVER);
+                }
+                else
+                {
+                    _currentLevel = Instantiate(levels[Level]);
+                    SwitchState(State.PLAY);
+                }
+                break;
+            case State.GAMEOVER:
+                panelGameOver.SetActive(true);
+                break;
+        }
+    }
+
+    void Update()
+    {
+        switch (_state)
+        {
+            case State.MENU:
+                break;
+            case State.INIT:
+                break;
+            case State.PLAY:
+                if (_currentLevel != null && _currentLevel.transform.childCount == 0 && !_isSwitchingState)
+                {
+                    SwitchState(State.LEVELCOMPLETED);
+                }
+                break;
+            case State.LEVELCOMPLETED:
+                break;
+            case State.LOADLEVEL:
+                break;
+            case State.GAMEOVER:
+                if (Input.anyKeyDown)
+                {
+                    SwitchState(State.MENU);
+                }
+                break;
+        }
+    }
+
+    void EndState()
+    {
+        switch (_state)
+        {
+            case State.MENU:
+                panelMenu.SetActive(false);
+                break;
+            case State.INIT:
+                break;
+            case State.PLAY:
+                break;
+            case State.LEVELCOMPLETED:
+                panelLevelCompleted.SetActive(false);
+                break;
+            case State.LOADLEVEL:
+                break;
+            case State.GAMEOVER:
+                panelPlay.SetActive(false);
+                panelGameOver.SetActive(false);
+                break;
+        }
+    }
+}
