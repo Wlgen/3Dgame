241912589a2a0db9e0d9a4b1cd513b9c4e9086ca
Arnau <arnaugesapascual1999@gmail.com>
2020-12-12 23:21:42 +0100

    Musiquita

--- Assets/Scripts/LevelGoal.cs
+++ Assets/Scripts/LevelGoal.cs
@@ -5,18 +5,21 @@ using UnityEngine;
 public class LevelGoal : MonoBehaviour
 {
     public ParticleSystem _confetti;
+    AudioSource _audioSource;
 
     private void Start()
     {
         _confetti.Stop();
+        _audioSource = GetComponent<AudioSource>();
     }
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Ball"))
         {
             Destroy(GameObject.FindGameObjectWithTag("Ball"));
+            _audioSource.Play();
             _confetti.Play();
-            Invoke("changeLevel", 5f);
+            Invoke("changeLevel", 4f);
         }
     }
 
