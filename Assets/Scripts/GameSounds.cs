39d41ffa666b2e735acaaaaa1c3c50cd3e1f96c1
Arnau <arnaugesapascual1999@gmail.com>
2020-12-13 02:01:33 +0100

    Bien REFACHERO

---
+++ Assets/Scripts/GameSounds.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameSounds : MonoBehaviour
+{
+
+    public AudioSource _mainTheme;
+    public AudioSource _titleTheme;
+
+    AudioSource _currentAudio;
+    public static GameSounds Instance { get; private set; }
+    void Start()
+    {
+        Instance = this;
+        _currentAudio = _titleTheme;
+        _currentAudio.Play();
+    }
+    public void setMainTheme()
+    {
+        if(_currentAudio != _mainTheme)
+        {
+            _currentAudio.Stop();
+            _currentAudio = _mainTheme;
+            _currentAudio.Play();
+        }
+    }
+
+    public void setTitleTheme()
+    {
+        if (_currentAudio != _titleTheme)
+        {
+            _currentAudio.Stop();
+            _currentAudio = _titleTheme;
+            _currentAudio.Play();
+        }
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
